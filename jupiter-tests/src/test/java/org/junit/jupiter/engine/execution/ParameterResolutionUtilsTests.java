4efe4851ecf968c5a667bff494cee63fd8a6a0ee
Stefano Cordio <stefano.cordio@gmail.com>
2025-07-19 16:14:22 +0200

    Set `DataFlowIssue` as NullAway suppression name alias

--- jupiter-tests/src/test/java/org/junit/jupiter/engine/execution/ParameterResolutionUtilsTests.java
+++ jupiter-tests/src/test/java/org/junit/jupiter/engine/execution/ParameterResolutionUtilsTests.java
@@ -148,7 +148,7 @@ class ParameterResolutionUtilsTests {
 		assertThat(arguments).containsExactly("something");
 	}
 
-	@SuppressWarnings({ "DataFlowIssue", "NullAway" })
+	@SuppressWarnings("DataFlowIssue")
 	@Test
 	void passContextInformationToParameterResolverMethods() {
 		anyTestMethodWithAtLeastOneParameter();
